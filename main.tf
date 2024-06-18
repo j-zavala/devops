e26e031e4bab93f4012925aedb898d7c90d35868
Johnny Zavala <jzavalafgz@gmail.com>
2024-06-18 17:05:53 -0400

    feat: configure Nginx reverse proxy with Terraform
    - Set up Nginx on the public EC2 instance as a reverse proxy to forward requests to the private instance
    - Added Terraform configuration for VPC, subnets, internet gateway, NAT gateway, and route tables
    - Created security groups and rules for public, private, and bastion instances
    - Configured public EC2 instance to serve as a reverse proxy using Nginx
    - Configured private EC2 instance to serve a simple HTML page using Nginx
    - Added user data scripts for public and private instances to automate Nginx setup

--- main.tf
+++ main.tf
@@ -2,7 +2,17 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.16"
+      version = "~> 5.54"
+    }
+
+    tls = {
+      source = "hashicorp/tls"
+      version = "~> 4.0"
+    }
+
+    local = {
+      source = "hashicorp/local"
+      version = "~> 2.5"
     }
 
     http = {
@@ -18,15 +28,32 @@ provider "aws" {
   region = "us-east-1"
 }
 
+provider "tls" {}
+
+provider "local" {}
+
 provider "http" {}
 
 data "http" "myip" {
   url = "https://api.ipify.org"
 }
 
-output "bastion_host_ip" {
-  value = format("${aws_instance.bastion_host.public_ip}/32")
+# output "bastion_host_ip" {
+#   value = format("${aws_instance.bastion_host.public_ip}/32")
+# }
+
+# output "my_ip" {
+  # value = format("${data.http.myip.response_body}/32")
+# }
+
+output "private_ip" {
+  value = aws_instance.private_instance.private_ip
+  # value = format("${aws_instance.private_instance.private_ip}/32")
 }
+
+# output "public_sg_id" {
+#   value = aws_security_group.public_sg.id
+# }
 
 resource "aws_vpc" "main" {
   cidr_block       = "10.0.0.0/24"
@@ -65,6 +92,27 @@ resource "aws_internet_gateway" "igw" {
   }
 }
 
+resource "aws_eip" "nat_eip" {
+  domain = "vpc"
+
+  tags = {
+    Name = "cwc-nat-eip"
+  }
+
+  depends_on = [aws_internet_gateway.igw]
+}
+
+resource "aws_nat_gateway" "nat_gw" {
+  subnet_id = aws_subnet.public_subnet.id
+  allocation_id = aws_eip.nat_eip.id
+
+  tags = {
+    Name = "cwc-nat-gw"
+  }
+
+  depends_on = [aws_internet_gateway.igw]
+}
+
 resource "aws_route_table" "public_route_table" {
   vpc_id = aws_vpc.main.id
 
@@ -86,6 +134,11 @@ resource "aws_route_table_association" "public_table_association" {
 resource "aws_route_table" "private_route_table" {
   vpc_id = aws_vpc.main.id
 
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat_gw.id
+  }
+
   tags = {
     Name = "cwc-private-route-table"
   }
@@ -96,9 +149,20 @@ resource "aws_route_table_association" "private_table_association" {
   route_table_id = aws_route_table.private_route_table.id
 }
 
+resource "tls_private_key" "private_key" {
+  algorithm = "RSA"
+  rsa_bits = 4096
+}
+
+resource "local_file" "private_key" {
+  filename = "${path.module}/cwc-key-pair.pem"
+  content = tls_private_key.private_key.private_key_pem
+  file_permission = "0600" # file is writable and readable only by the owner
+}
+
 resource "aws_key_pair" "key" {
   key_name = "cwc-key"
-  public_key = file("./cwc-key-pair.pem.pub")
+  public_key = tls_private_key.private_key.public_key_openssh
 }
 
 resource "aws_security_group" "public_sg" {
@@ -144,58 +208,16 @@ resource "aws_instance" "public_instance" {
   key_name                    = aws_key_pair.key.key_name
   vpc_security_group_ids      = [aws_security_group.public_sg.id]
 
+  user_data = templatefile("public_ec2_nginx_setup.sh.tpl", {
+    private_ip = aws_instance.private_instance.private_ip
+    # private_ip = format("${aws_instance.private_instance.private_ip}/32")
+  })
+
   tags = {
     Name = "cwc-public-ec2"
   }
-}
-####################### PRIVATE SG & EC2 #######################
 
-resource "aws_security_group" "private_sg" {
-  name = "cwc-private-sg" # This is the actual name of the security group in AWS
-  description = "Allow HTTP from public subnet, SSH from bastion host, all outbound traffic"
-  vpc_id = aws_vpc.main.id
-
-  tags = {
-    Name = "cwc-private-sg" # This is a tag named "Name" with the value "private_sg"
-  }
-}
-
-resource "aws_vpc_security_group_ingress_rule" "allow_http_inbound_from_public_subnet" {
-  security_group_id = aws_security_group.private_sg.id
-  cidr_ipv4 = "10.0.0.0/25"
-  from_port         = 80
-  to_port           = 80
-  ip_protocol       = "tcp"
-  description = "Allow HTTP inbound traffic from public subnet"
-}
-
-resource "aws_vpc_security_group_ingress_rule" "allow_ssh_inbound_from_bastion_host" {
-  security_group_id = aws_security_group.private_sg.id
-  cidr_ipv4 = format("${aws_instance.bastion_host.public_ip}/32")
-  from_port         = 22
-  to_port           = 22
-  ip_protocol       = "tcp"
-  description = "Allow SSH inbound traffic from bastion host"
-}
-
-resource "aws_vpc_security_group_egress_rule" "private_allow_all_outbound_rule" {
-  security_group_id = aws_security_group.private_sg.id
-  cidr_ipv4 = "0.0.0.0/0"
-  ip_protocol       = "-1" # This means all protocols
-  description = "Allow all outbound traffic"
-}
-
-resource "aws_instance" "private_instance" {
-  ami                         = "ami-08a0d1e16fc3f61ea"
-  instance_type               = "t2.micro"
-  subnet_id                   = aws_subnet.private_subnet.id
-  associate_public_ip_address = false
-  key_name                    = aws_key_pair.key.key_name
-  vpc_security_group_ids      = [aws_security_group.private_sg.id]
-
-  tags = {
-    Name = "cwc-private-ec2"
-  }
+   depends_on = [aws_instance.private_instance]
 }
 
 ####################### BASTION SG & EC2 #######################
@@ -237,4 +259,57 @@ resource "aws_instance" "bastion_host" {
   tags = {
     Name = "cwc-bastion-host-ec2"
   }
+}
+
+####################### PRIVATE SG & EC2 #######################
+
+resource "aws_security_group" "private_sg" {
+  name = "cwc-private-sg" # This is the actual name of the security group in AWS
+  description = "Allow HTTP from public subnet, SSH from bastion host, all outbound traffic"
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "cwc-private-sg" # This is a tag named "Name" with the value "private_sg"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_http_inbound_from_public_subnet" {
+  security_group_id = aws_security_group.private_sg.id
+  referenced_security_group_id  = aws_security_group.public_sg.id
+  # cidr_ipv4 = "10.0.0.0/25"
+  from_port         = 80
+  to_port           = 80
+  ip_protocol       = "tcp"
+  description = "Allow HTTP inbound traffic from public subnet"
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_ssh_inbound_from_bastion_host" {
+  security_group_id = aws_security_group.private_sg.id
+  referenced_security_group_id = aws_security_group.bastion_sg.id
+  from_port         = 22
+  to_port           = 22
+  ip_protocol       = "tcp"
+  description = "Allow SSH inbound traffic from bastion host"
+}
+
+resource "aws_vpc_security_group_egress_rule" "private_allow_all_outbound_rule" {
+  security_group_id = aws_security_group.private_sg.id
+  cidr_ipv4 = "0.0.0.0/0"
+  ip_protocol       = "-1" # This means all protocols
+  description = "Allow all outbound traffic"
+}
+
+resource "aws_instance" "private_instance" {
+  ami                         = "ami-08a0d1e16fc3f61ea"
+  instance_type               = "t2.micro"
+  subnet_id                   = aws_subnet.private_subnet.id
+  associate_public_ip_address = false
+  key_name                    = aws_key_pair.key.key_name
+  vpc_security_group_ids      = [aws_security_group.private_sg.id]
+
+  tags = {
+    Name = "cwc-private-ec2"
+  }
+
+  user_data = file("private_ec2_nginx_setup.sh.tpl")
 }
