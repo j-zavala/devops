7a5cf29d68a9799df63225094c7d101693803c7a
Johnny Zavala <jzavalafgz@gmail.com>
2024-06-22 22:24:56 -0400

    feat: Added application load balancer and associated resources

--- main.tf
+++ main.tf
@@ -39,7 +39,7 @@ module "vpc" {
 
 resource "aws_security_group" "private_sg" {
   name        = "${var.app_name}-private-sg"
-  description = "Allow HTTP from public subnet, SSH from bastion host, all outbound traffic"
+  description = "Allow HTTP from public subnet, all outbound traffic"
   vpc_id      = module.vpc.vpc_id
 
   tags = {
@@ -47,16 +47,16 @@ resource "aws_security_group" "private_sg" {
   }
 }
 
-resource "aws_vpc_security_group_ingress_rule" "allow_http_inbound_from_public_subnet" {
-  security_group_id = aws_security_group.private_sg.id
-  cidr_ipv4         = "0.0.0.0/0"
-  from_port         = 80
-  to_port           = 80
-  ip_protocol       = "tcp"
-  description       = "Allow HTTP inbound traffic from public subnet"
+resource "aws_vpc_security_group_ingress_rule" "private_allow_http_inbound_from_public_subnet" {
+  security_group_id            = aws_security_group.private_sg.id
+  referenced_security_group_id = aws_security_group.load_balancer_sg.id
+  from_port                    = 80
+  to_port                      = 80
+  ip_protocol                  = "tcp"
+  description                  = "Allow HTTP inbound traffic from public subnet"
 }
 
-resource "aws_vpc_security_group_egress_rule" "private_allow_all_outbound_rule" {
+resource "aws_vpc_security_group_egress_rule" "private_allow_all_outbound" {
   security_group_id = aws_security_group.private_sg.id
   cidr_ipv4         = "0.0.0.0/0"
   ip_protocol       = "-1"
@@ -108,4 +108,66 @@ resource "aws_iam_role_policy_attachment" "private_ec2_policy_attachment" {
 resource "aws_iam_instance_profile" "private_ec2_instance_profile" {
   name = "${var.app_name}-private-ec2-instance-profile"
   role = aws_iam_role.private_ec2_role.name
+}
+
+resource "aws_lb" "load_balancer" {
+  name               = "${var.app_name}-load-balancer"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.load_balancer_sg.id]
+  subnets            = module.vpc.public_subnet_ids
+}
+
+resource "aws_vpc_security_group_ingress_rule" "lb_allow_http_inbound_from_internet" {
+  security_group_id = aws_security_group.load_balancer_sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 80
+  to_port           = 80
+  ip_protocol       = "tcp"
+  description       = "Allow HTTP inbound traffic from internet"
+}
+
+
+resource "aws_vpc_security_group_egress_rule" "lb_allow_all_outbound_to_private_instances" {
+  security_group_id = aws_security_group.load_balancer_sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1"
+  description       = "Allow all outbound traffic to private instances"
+}
+
+resource "aws_security_group" "load_balancer_sg" {
+  name        = "${var.app_name}-load-balancer-sg"
+  description = "Allows inbound HTTP from internet; then, outbound to private instances for request forwarding"
+  vpc_id      = module.vpc.vpc_id
+
+  tags = {
+    Name = "${var.app_name}-load-balancer-sg"
+  }
+}
+
+# Target group
+resource "aws_lb_target_group" "target_group" {
+  name     = "${var.app_name}-target-group"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = module.vpc.vpc_id
+}
+
+# Attach instances to target group
+resource "aws_lb_target_group_attachment" "private_instance_attachment" {
+  count            = length(aws_instance.private_instance)
+  target_group_arn = aws_lb_target_group.target_group.arn
+  target_id        = aws_instance.private_instance[count.index].id
+  port             = 80
+}
+
+resource "aws_lb_listener" "listener" {
+  port              = 80
+  protocol          = "HTTP"
+  load_balancer_arn = aws_lb.load_balancer.arn
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.target_group.arn
+  }
 }
